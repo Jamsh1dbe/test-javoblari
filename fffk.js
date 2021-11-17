0db533876dcef3fbd158751ac2f4a651be7f8b00
jamsh1dbe <jamshidbeksharipov2@gmail.com>
2021-11-17 14:42:41 +0500

    test

---
+++ fffk.js
@@ -0,0 +1,62 @@
+
+//1.
+// console.log(parseInt(100,3))   //javobi nechi chiqadi 
+// javob: 9
+// 2.“ “ ni ishiga “” ni qiqish qanday bo`ladi?
+// "\"jamshid\""
+// 3.Stringni o`rtasiga string tiqish qanday boladi?
+// `asfasfasf,$'jamshid' fasfasf`
+// 4.Arrow funksiya qanday yasaladi?
+// const kk =()=>{}
+// 5.||    qiymat berildi qaysi holatda true chiqadi?
+//     2 ovidan biri true bolsa true
+// 6.Math.abs() nima chiqadi?
+// mudul
+// 7. Fill nechda parametr oladi?
+// 3ta
+// 8.
+// ar= [ 1,2,3 ,4,'besh',6]
+// arr = ar.slice(0,3)
+// console.log(arr.splice(1,2))
+// [2,3]
+// 9.tofixed(-1)oladimi?
+// yoq
+// 10.  eval() nima?
+// arifmetik amal bajaradi stringda
+// 11. 
+// for( let i=0; i<-10; i--){
+// console.log(i)}       
+//  javobi nima?
+// ishlamaydi
+// 12. join()vazifasi nima?
+//stringa otkazadi
+// 13.sortni 10 katta sonlada ishlash uchun nima qilinadi?
+// sort((a,b)=>a-b)
+// 14. a=[10,100]
+// console.log((Array.from(a,x=> x+x)));
+// javobi nima
+// [20,200]
+// 15.every() dan bizga nima qaytadi?
+//true false
+// 16.(…) vazifasi nima?
+// arrayga kirish
+// 17.localCompare() nima?
+//taqqoshlash
+// 18.trim() vazifasi?
+//boshidan va oxiridan boshliqni ochiradi
+// 19. && qiymatlar berilsa 2 ta 1 tasi true 2 chisi false bolsa nima javob chiqadi?
+// false
+// 20.indexOf()ni 2 parametri nima?
+// dan keying
+// 21.randomdan chiqqan natijani butunini olish uchun nima ishlatamiz?
+// parseInt()
+// 22. var a=12.5
+// console.log(a.toFixed(0));
+// nima javob chiqadi.
+// 13
+// 23. fill()ni 2chi parametric nomi
+//   dan
+// 24.typeofnima ?
+// taypini chiqaradi
+// 25. if ga (agar teng bolmasa) degan qiymatni qanday yozamiz?
+// !=
